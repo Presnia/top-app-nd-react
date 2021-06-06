ec6e956d73bc4f9a736dca299f1329d1b6010925
Andrei Dzebysh <presnia@yahoo.com>
2021-06-06 22:42:37 +0300

    Created and styled the Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Button from '../components/Button/Button';
 import Htag from '../components/Htag/Htag';
 
 
@@ -6,6 +7,8 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Text</Htag>
+      <Button appearance='primary' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
     </>
   );
 }
