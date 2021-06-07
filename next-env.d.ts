4e18e8d1834a708637504797a9470f390e3f0911
Andrei Dzebysh <presnia@yahoo.com>
2021-06-07 21:26:22 +0300

    Created the Rating component with all functionality. Adjusted .eslintrc

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,6 +2,11 @@
 /// <reference types="next/types/global" />
 
 declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGAElement>>;
+  export default content;
+}
+
+declare module "classnames" {
   const content: any;
   export default content;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import { Htag, Button, P, Tag } from '../components/';
+import { Htag, Button, P, Tag, Rating } from '../components/';
+import {useEffect, useState} from "react";
 
 
 export default function Home(): JSX.Element {
+    const [rating, setRating] = useState<number>(4);
   
   return (
     <>
-      <Htag tag="h1">Text</Htag>
+      <Htag tag="h1">Title</Htag>
       <Button appearance='primary' arrow='right'>Button</Button>
       <Button appearance='ghost' arrow='down'>Button</Button>
       <P size='l'>Large</P>
@@ -15,6 +17,7 @@ export default function Home(): JSX.Element {
       <Tag size='l' color='red'>Red</Tag>
       <Tag size='m' color='green'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
+      <Rating rating={rating} isEditable={true} setRating={setRating}  />
     </>
   );
 }
