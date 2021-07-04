8f5de45fbd0269679a085b1b6d77efbd24698829
Andrei Dzebysh <presnia@yahoo.com>
2021-07-04 16:47:22 +0300

    Used regex to get the correct price in rubles

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -1,7 +1,8 @@
 import    { HhDataProps } from './HhData.props';
 import RateIcon from './rate.svg';
-import s from './HhData.module.css';
 import { Card } from "../Card/Card";
+import { priceRu } from "../../helpers/helpers";
+import s from './HhData.module.css';
 
 export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDataProps): JSX.Element => {
   return (
@@ -13,7 +14,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
         <Card className={s.salary}>
           <div>
               <div className={s.title}>Начальный</div>
-              <div className={s.salaryValue}>{juniorSalary}</div>
+              <div className={s.salaryValue}>{priceRu(juniorSalary)}</div>
               <div className={s.rate}>
                   <RateIcon className={s.filled}/>
                   <RateIcon />
@@ -22,7 +23,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
           </div>
             <div>
                 <div className={s.title}>Средний</div>
-                <div className={s.salaryValue}>{middleSalary}</div>
+                <div className={s.salaryValue}>{priceRu(middleSalary)}</div>
                 <div className={s.rate}>
                     <RateIcon className={s.filled}/>
                     <RateIcon className={s.filled}/>
@@ -31,7 +32,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
             </div>
             <div>
                 <div className={s.title}>Профессионал</div>
-                <div className={s.salaryValue}>{seniorSalary}</div>
+                <div className={s.salaryValue}>{priceRu(seniorSalary)}</div>
                 <div className={s.rate}>
                     <RateIcon className={s.filled}/>
                     <RateIcon className={s.filled}/>
