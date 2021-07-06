64439ed4135a7b1d298e6a41652b4c8b48f7a82f
Andrei Dzebysh <presnia@yahoo.com>
2021-07-06 21:29:46 +0300

    The Product component has been styled

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -2,39 +2,71 @@ import { ProductProps } from './Product.props';
 import { Card } from "../Card/Card";
 import { Rating } from "../Rating/Rating";
 import { Tag } from "../Tag/Tag";
+import { Button } from "../Button/Button";
+import {declOfNumber, priceRu} from "../../helpers/helpers";
+import { Divider } from "../Divider/Divider";
 import cn from 'classnames';
 import s from './Product.module.css';
-import {Button} from "../Button/Button";
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
     return (
         <Card className={s.product}>
             <div className={s.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/></div>
             <div className={s.title}>{product.title}</div>
-            <div className={s.price}>{product.price}</div>
-            <div className={s.credit}>{product.credit}</div>
+            <div className={s.price}>
+                {priceRu(product.price)}
+                {product.oldPrice &&
+                <Tag className={s.sale}
+                     color='green'>{priceRu(product.price - product.oldPrice)}
+                </Tag>}
+            </div>
+            <div className={s.credit}>
+                {priceRu(product.credit)}
+                <span className={s.month}>/мес</span>
+            </div>
             <div className={s.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
-            <div className={s.tags}>{product.categories.map(c => <Tag key={c} color='ghost'>{c}</Tag>)}</div>
+            <div className={s.tags}>{product.categories.map(c =>
+                <Tag className={s.category} key={c}
+                     color='ghost'>
+                    {c}
+                </Tag>
+            )}
+            </div>
             <div className={s.priceTitle}>цена</div>
             <div className={s.creditTitle}>кредит</div>
-            <div className={s.rateTitle}>{product.reviewCount} отзывов</div>
-            <div className={s.hr}><hr/></div>
-            <div className={s.description}>{product.description}</div>
-            <div className={s.features}>features</div>
-            <div className={s.advBlock}>
-                <div>
-                    <div className={s.advantages}>Преимущества</div>
-                    <div>{product.advantages}</div>
-                </div>
-                <div>
-                    <div className={s.disadvantages}>Недостатки</div>
-                    <div>{product.disadvantages}</div>
-                </div>
+            <div className={s.rateTitle}>
+                {product.reviewCount} {declOfNumber(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
             </div>
-            <div className={s.hr}><hr/></div>
+            <Divider className={s.hr} />
+            <div className={s.description}>{product.description}</div>
+            <div className={s.features}>
+                {product.characteristics.map(c => (
+                    <div key={c.name} className={s.characteristics}>
+                        <span className={s.characteristicsName}>{c.name}</span>
+                        <span className={s.characteristicsDots}></span>
+                        <span className={s.characteristicsValue}>{c.value}</span>
+                    </div>
+                ))}
+            </div>
+            <div className={s.advBlock}>
+                {product.advantages &&
+                <div className={s.advantages}>
+                    <div className={s.advTitle}>Преимущества</div>
+                    <div>{product.advantages}</div>
+                </div>}
+                {product.disadvantages &&
+                <div className={s.disadvantages}>
+                    <div className={s.advTitle}>Недостатки</div>
+                    <div>{product.disadvantages}</div>
+                </div>}
+            </div>
+            <Divider className={s.hr} />
             <div className={s.actions}>
                 <Button appearance='primary'>Узнать подробнее</Button>
-                <Button appearance='ghost' arrow={'right'}>Читать отзывы</Button>
+                <Button appearance='ghost'
+                        className={s.reviewBtn}
+                        arrow={'right'}>Читать отзывы
+                </Button>
             </div>
         </Card>
     );
