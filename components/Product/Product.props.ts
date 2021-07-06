d6d73a5d0034bc17328fabd482a237376d5d00a5
Andrei Dzebysh <presnia@yahoo.com>
2021-07-06 16:46:13 +0300

    The Product component is created

--- components/Product/Product.props.ts
+++ components/Product/Product.props.ts
@@ -1,6 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import { ProductModel } from '../../interfaces/product.interface';
 
-export interface SearchProps
+export interface ProductProps
         extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-
+          product: ProductModel;
         }
