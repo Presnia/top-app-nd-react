0629526b7e7e67202db7eeeb9fe430f180146acd
Andrei Dzebysh <presnia@yahoo.com>
2021-07-11 11:59:57 +0300

    Implemented error handling for the Rating component

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -2,10 +2,10 @@ import  { RatingProps } from './Rating.props';
 import {useEffect, useState, KeyboardEvent, forwardRef, ForwardedRef} from "react";
 import StarIcon from './star.svg';
 import cn from 'classnames';
-import styles from './Rating.module.css';
+import s from './Rating.module.css';
 
 
-export const Rating = forwardRef(({ isEditable = false, rating, setRating, children, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+export const Rating = forwardRef(({ isEditable = false, rating, error, setRating, children, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
     const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
     useEffect(() => {
@@ -16,9 +16,9 @@ export const Rating = forwardRef(({ isEditable = false, rating, setRating, child
         const updatedArray = ratingArray.map((r: JSX.Element, i:number) => {
           return (
               <span
-                  className={cn(styles.star, {
-                      [styles.filled]: i < currentRating,
-                      [styles.editable]: isEditable
+                  className={cn(s.star, {
+                      [s.filled]: i < currentRating,
+                      [s.editable]: isEditable
                   })}
                   onMouseEnter={() => changeDisplay(i + 1)}
                   onMouseLeave={() => changeDisplay(rating)}
@@ -56,8 +56,11 @@ export const Rating = forwardRef(({ isEditable = false, rating, setRating, child
     };
 
   return (
-    <div {...props} ref={ref}>
+    <div className={cn(s.ratingWrapper, {
+        [s.error]: error
+    })} {...props} ref={ref}>
         {ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+        {error && <span className={s.errorMessage}>{error.message}</span>}
     </div>
   );
 });
