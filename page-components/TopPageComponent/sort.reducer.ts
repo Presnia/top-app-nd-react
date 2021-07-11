ad333fd193873b342946e54a391d8dda79ab37bf
Andrei Dzebysh <presnia@yahoo.com>
2021-07-11 13:50:50 +0300

    The sort reducer is modified and fixed bug with renewing a products

--- page-components/TopPageComponent/sort.reducer.ts
+++ page-components/TopPageComponent/sort.reducer.ts
@@ -1,7 +1,7 @@
-import { SortEnum } from "../../components/Sort/Sort.props";
-import { ProductModel } from "../../interfaces/product.interface";
+import {SortEnum} from "../../components/Sort/Sort.props";
+import {ProductModel} from "../../interfaces/product.interface";
 
-export type SortActions = { type: SortEnum.Rating } | { type: SortEnum.Price };
+export type SortActions = { type: SortEnum.Rating } | { type: SortEnum.Price } | { type: 'reset', initialState: ProductModel[] };
 
 export interface SortReducerState {
     sort: SortEnum;
@@ -23,6 +23,11 @@ export const sortReducer = (state: SortReducerState, action: SortActions) => {
                 products: state
                     .products.
                     sort((a, b) => a.price > b.price ? 1 : -1)
+            };
+        case 'reset':
+            return {
+                sort: SortEnum.Rating,
+                products: action.initialState
             };
         default:
             throw new Error('Неверный тип сортировки');
