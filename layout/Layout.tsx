9194ad9eaadf03a715a579347648d70bae84afdf
Andrei Dzebysh <presnia@yahoo.com>
2021-07-26 20:09:51 +0300

    Implemented correct accessibility from the keyboard

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,18 +2,38 @@ import { LayoutProps } from './Layout.props';
 import { Header } from './Header/Header';
 import { SideBar } from "./SideBar/SideBar";
 import { Footer } from "./Footer/Footer";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState, KeyboardEvent, useRef } from "react";
 import { AppContextProvider, IAppContext } from "../context/app.context";
 import { Up } from "../components";
+import cn from 'classnames';
 import s from './Layout.module.css';
 
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+    const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false);
+    const bodyRef = useRef<HTMLDivElement>(null);
+
+    const scipContentAction = (key: KeyboardEvent, ) => {
+        if (key.code === 'Space' || key.code === 'Enter') {
+            key.preventDefault();
+            bodyRef.current?.focus();
+        }
+        setIsSkipLinkDisplayed(false);
+    };
+
     return (
         <div className={s.wrapper}>
+            <a
+                onFocus={() => setIsSkipLinkDisplayed(true)}
+                tabIndex={1}
+                className={cn(s.skipLink , {
+                [s.displayed]: isSkipLinkDisplayed
+                })}
+                onKeyDown={scipContentAction}
+            >Сразу к содержанию</a>
             <Header className={s.header} />
             <SideBar className={s.sidebar} />
-            <div className={s.body}>
+            <div className={s.body} ref={bodyRef} tabIndex={0}>
                   {children}
             </div>
             <Footer className={s.footer} />
--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, KeyboardEvent } from "react";
 import { AppContext } from "../../context/app.context";
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interfaces';
 import Link from 'next/link';
@@ -19,6 +19,13 @@ export const Menu = (): JSX.Element => {
             }
             return m;
         }));
+    };
+
+    const openSecondLevelKey = (key: KeyboardEvent, secondCategory: string) => {
+        if (key.code === 'Space' || key.code === 'Enter') {
+            key.preventDefault();
+            openSecondLevel(secondCategory);
+        }
     };
 
     const variants = {
@@ -76,7 +83,10 @@ export const Menu = (): JSX.Element => {
                     }
                     return (
                         <div key={m._id.secondCategory}>
-                            <div className={s.secondLevel} onClick={
+                            <div
+                                tabIndex={0}
+                                onKeyDown={(key: KeyboardEvent) => openSecondLevelKey(key, m._id.secondCategory)}
+                                className={s.secondLevel} onClick={
                                 () => openSecondLevel(m._id.secondCategory)}
                             >
                                 {m._id.secondCategory}
@@ -88,7 +98,7 @@ export const Menu = (): JSX.Element => {
                                 variants={variants}
                                 className={cn(s.secondLevelBlock)}
                             >
-                                {buildThirdLevel(m.pages, menuItem.route)}
+                                {buildThirdLevel(m.pages, menuItem.route, m.isOpened ?? false)}
                             </motion.div>
                         </div>
                     );
@@ -97,12 +107,12 @@ export const Menu = (): JSX.Element => {
         );
     };
 
-    const buildThirdLevel = (pages: PageItem[], route: string) => {
+    const buildThirdLevel = (pages: PageItem[], route: string, isOpened: boolean) => {
         return (
             pages.map(p => (
                 <motion.div key={p._id} variants={variantsChildren}>
                     <Link href={`/${route}/${p.alias}`}>
-                        <a className={cn(s.thirdLevel, {
+                        <a tabIndex={isOpened ? 0 : -1} className={cn(s.thirdLevel, {
                             [s.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
                         })}>
                             {p.category}
