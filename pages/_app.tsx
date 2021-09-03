f44b56846bdaa192bdbbb5ec63ff93a9bcf54569
Presnia <presnia@yahoo.com>
2021-09-03 18:04:41 +0300

    Added meta data to the pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import Head from 'next/head';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
     <>
       <Head>
-        <title>My Top App</title>
-        <link rel="icon" href="/favicon.ico" />
+          <title>My Top App</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+          <meta property='og:locale' content='ru_RU' />
       </Head>
       <Component {...pageProps} />
     </>
