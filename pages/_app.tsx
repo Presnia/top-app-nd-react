2203d663b2c6933a16ca3c3a818e140d0bd9215a
Presnia <presnia@yahoo.com>
2021-09-03 18:41:44 +0300

    Corrected code to fix router functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import Head from 'next/head';
+import Router from 'next/router';
 import ym from 'react-yandex-metrika';
 import { YMInitializer } from "react-yandex-metrika";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
-    router.events.on('routeChangeComplete', (url: string) => {
+    Router.events.on('routeChangeComplete', (url: string) => {
        if (typeof window !== 'undefined') {
            ym('hit', url);
        }
